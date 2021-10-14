7cbddd81c03df239df639a323bcc8f8516c05a4e
Anusha98s <92158408+Anusha98s@users.noreply.github.com>
2021-10-14 10:33:00 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Construct_Day1
+{
+    class LMS
+
+    {
+        string name;
+        int SNO;
+        long mobilenum;
+        string email;
+
+        public void Display()
+        {
+            Console.WriteLine("Iplementation void");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Basic_Construct_Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write("Wwhat is your fav snack\n");
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine("How many plates you can have it at a time?\n");
+            int count = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(name);
+            Console.WriteLine(count);
+            if (count >= 2)
+            {
+                Console.WriteLine("Too much of snacking is not a healthy sign....!!");
+            }
+        }
+    }
+}
